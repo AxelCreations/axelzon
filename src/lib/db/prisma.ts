982e9cdf4f6ca98fa479606fe1f9f857ee019515
Reynel Axel Perdomo <ReynelAxel@gmail.com>
2023-10-28 15:05:22 -0400

    Added prisma extension

--- src/lib/db/prisma.ts
+++ src/lib/db/prisma.ts
@@ -10,8 +10,28 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientSingleton | undefined;
 }
 
-const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
+const prismaBase = globalForPrisma.prisma ?? prismaClientSingleton();
 
-export default prisma
+const prisma = prismaBase.$extends({
+  query: {
+    cart: {
+      async update({ args, query }) {
+        args.data = { ...args.data, updatedAt: new Date() };
+        return query(args);
+      }
+    }
+  }
+}) as PrismaClient;
+
+export default prisma.$extends({
+  query: {
+    cart: {
+      async update({ args, query }) {
+        args.data = { ...args.data, updatedAt: new Date() };
+        return query(args);
+      }
+    }
+  }
+});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
